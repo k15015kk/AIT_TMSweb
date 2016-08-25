b98d6fc9741db0fae7d137912c6038e973d4b248
k15015kk <k15015kk@set-iMac-2.local>
2016-08-25 18:31:14 +0900

    JavaScript and testcss adds ver 2.06

--- js/index.js
+++ js/index.js
@@ -1,8 +1,38 @@
 var flag = 1;
 var csvList = new Array();
 var dataList = new Array();
+var redColorList = ['#FFEBEE','#FFCDD2','#EF9A9A','#E57373','#EF5350','#F44336','#E53935','#D32F2F','#C62828','#B71C1C'];
+var blueColorList = ['#E3F2FD','#BBDEFB','#90CAF9','#64B5F6','#42A5F5','#2196F3','#1E88E5','#1976D2','#1565C0','#0D47A1'];
+var greenColorList = ['#E8F5E9','#C8E6C9','#A5D6A7','#81C784','#66BB6A','#4CAF50','#43A047','#388E3C','#2E7D32','#1B5E20'];
 
 $(function(){
+
+    function item001Flag () {
+        if (dataList[dataList.length-2][2] <= 0) {
+                $('#value').css('background',redColorList[0]);
+            } else if(dataList[dataList.length-2][2] > 0 && dataList[dataList.length-2][2] <= 5) {
+                $('#value').css('background',redColorList[1]);                
+            } else if(dataList[dataList.length-2][2] > 5 && dataList[dataList.length-2][2] <= 10) {
+                $('#value').css('background',redColorList[2]);
+            } else if(dataList[dataList.length-2][2] > 10 && dataList[dataList.length-2][2] <= 15) {
+                $('#value').css('background',redColorList[3]);
+            } else if(dataList[dataList.length-2][2] > 15 && dataList[dataList.length-2][2] <= 20) {
+                $('#value').css('background',redColorList[4]);
+            } else if(dataList[dataList.length-2][2] > 20 && dataList[dataList.length-2][2] <= 25) {
+                $('#value').css('background',redColorList[5]);
+            } else if(dataList[dataList.length-2][2] > 25 && dataList[dataList.length-2][2] <= 30) {
+                $('#value').css('background',redColorList[6]);
+            } else if(dataList[dataList.length-2][2] > 30 && dataList[dataList.length-2][2] <= 35) {
+                $('#value').css('background',redColorList[7]);
+            } else if(dataList[dataList.length-2][2] > 35 && dataList[dataList.length-2][2] <= 40) {
+                $('#value').css('background',redColorList[8]);
+            } else if(dataList[dataList.length-2][2] > 40) {
+                $('#value').css('background',redColorList[9]);
+            } else {
+                $('#value').css('background',redColorList[5]);                
+            }
+    }
+
     $.ajax({
         url:'csv/20160823.csv',
         success: function(data) {
@@ -13,6 +43,8 @@ $(function(){
             }
 
             $('#value').text(dataList[dataList.length-2][2]);
+            
+            item001Flag();
         }
     });
 
@@ -64,6 +96,7 @@ $(function(){
         $('#item002').css('background','#bdbdbd');
         $('#item003').css('background','#bdbdbd');
         $('#value').text(dataList[dataList.length-2][2]);
+        
     });
 
     $('#item002').click(function() {
